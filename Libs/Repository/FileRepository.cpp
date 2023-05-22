bacdfc952130c6689df7ee7902132f6c5d660c62
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-05-22 19:14:43 +0300

    Added logic to parse format in file

--- Libs/Repository/FileRepository.cpp
+++ Libs/Repository/FileRepository.cpp
@@ -1,1 +1,74 @@
+#include <fstream>
 #include "FileRepository.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////PRIVATE/////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::string FileRepository::m_findFirstVariable(const std::string &variable, bool &outFound)
+{
+    std::string result;
+    while(std::getline(m_inputFile, result) and not result.starts_with(variable));
+
+    outFound = result.length() >= variable.length();
+
+    if(outFound)
+    {
+        //we remove the variable flag, e.g. ARTIST="
+        result.erase(result.begin(), result.begin() + variable.length());
+
+        std::string line;
+        //while the line does not end with _"_ right before the "Carriage Return" if it exists, we need to continue reading
+        while((result.at(result.length() - 2) != '"' and result.at(result.length() - 1) != '"') and std::getline(m_inputFile, line) )
+            result += ' ' + line;
+
+        //we delete the \r = "Carriage Return" in this case if it exists at the end of the file and append a space
+        if (result.at(result.length() - 1) != '"')
+            result.erase(result.length() - 1);
+
+
+        //we remove the last _"_
+        result.erase(result.length() - 1);
+    }
+
+
+    return result;
+}
+
+
+
+
+Song FileRepository::m_getNextSong()
+{
+    bool found1, found2, found3;
+    std::string artist = m_findFirstVariable(ARTIST_VAR, found1);
+    std::string title = m_findFirstVariable(TITLE_VAR, found2);
+    std::string lyrics = m_findFirstVariable(LYRICS_VAR, found3);
+
+    if (found1 and found2 and found3)
+        return Song(artist, title, lyrics);
+    else
+        throw std::runtime_error("Could not load song, check format in file!");
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////PUBLIC//////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FileRepository::FileRepository(std::string filepath) : m_filepath(std::move(filepath))
+{
+
+}
+
+void FileRepository::load()
+{
+    std::ifstream inputFile;
+    inputFile.open(m_filepath);
+    if (inputFile.fail())
+        throw std::runtime_error("Failed to open file!");
+
+
+    inputFile.close();
+}
--- Libs/Repository/FileRepository.h
+++ Libs/Repository/FileRepository.h
@@ -1,12 +1,20 @@
 #pragma once
+#include "Repository.h"
 
-
-class FileRepository
+class FileRepository : public Repository
 {
 private:
+    std::string m_filepath;
 
+    static constexpr char ARTIST_VAR[] = "ARTIST=\"";
+    static constexpr char TITLE_VAR[] = "TITLE=\"";
+    static constexpr char LYRICS_VAR[] = "LYRICS=\"";
+
+    std::string m_findFirstVariable(const std::string &variable, bool &outFound);
+    Song m_getNextSong();
 public:
-    FileRepository() = default;
-
+    explicit FileRepository(std::string filepath);
+    void load();
+    void save();
 
 };
