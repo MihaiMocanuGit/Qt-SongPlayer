5a176aba96151fb011ebfe1cfa0a5fd1bb99c338
MihaiMocanu-FMI <mihai.mocanu1@stud.ubbcluj.ro>
2023-05-24 18:23:36 +0300

    implementation of apply

--- Libs/Controller/ActionAdd.cpp
+++ Libs/Controller/ActionAdd.cpp
@@ -1,1 +1,6 @@
 #include "ActionAdd.h"
+
+void ActionAdd::apply()
+{
+    m_ref_repo.remove(m_song);
+}
--- Libs/Controller/ActionDelete.cpp
+++ Libs/Controller/ActionDelete.cpp
@@ -1,2 +1,7 @@
 
 #include "ActionDelete.h"
+
+void ActionDelete::apply()
+{
+    m_ref_repo.store(m_song);
+}
